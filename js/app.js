77efc70f513e01f14074ce18edfa6c81f16b3042
aapiane09 <aapiane09@gmail.com>
2016-12-04 21:50:01 -0800

    A little styling, tried to start winCheck function

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
   // givens for starting tic tac toe
   var playerTurn = 'X';
-  console.log(playerTurn);
+  console.log(playerTurn + " goes first.");
 
   // grab all divs as an array-like object and iterate
   $('.box').each(function (){
@@ -24,6 +24,8 @@ $(document).ready(function() {
         console.log("Player Turn is " + playerTurn);
         $(this).text(playerTurn);
 
+        // Check board to see if win conditions are satisfied
+
         // switch playerTurn value for next playerTurn
         if (playerTurn === 'X') {
 
@@ -33,7 +35,6 @@ $(document).ready(function() {
           playerTurn = 'X';
           }
         }
-        checkWin();
       })
     })
 
@@ -49,7 +50,7 @@ $(document).ready(function() {
 
   })
 
-var winArray = [[$('.box').eq(0).text(), $('.box').eq(1).text(), $('.box').eq(2).text()],
+var winningCombos = [[$('.box').eq(0).text(), $('.box').eq(1).text(), $('.box').eq(2).text()],
                 [$('.box').eq(3).text(), $('.box').eq(4).text(), $('.box').eq(5).text()],
                 [$('.box').eq(6).text(), $('.box').eq(7).text(), $('.box').eq(8).text()],
                 [$('.box').eq(0).text(), $('.box').eq(3).text(), $('.box').eq(6).text()],
@@ -57,39 +58,82 @@ var winArray = [[$('.box').eq(0).text(), $('.box').eq(1).text(), $('.box').eq(2)
                 [$('.box').eq(2).text(), $('.box').eq(5).text(), $('.box').eq(8).text()],
                 [$('.box').eq(0).text(), $('.box').eq(4).text(), $('.box').eq(8).text()],
                 [$('.box').eq(2).text(), $('.box').eq(4).text(), $('.box').eq(6).text()]];
-var oWin = [[$('.box').eq(0).text('O'), $('.box').eq(1).text('O'), $('.box').eq(2).text('O')],
-                [$('.box').eq(3).text('O'), $('.box').eq(4).text('O'), $('.box').eq(5).text('O')],
-                [$('.box').eq(6).text('O'), $('.box').eq(7).text('O'), $('.box').eq(8).text('O')],
-                [$('.box').eq(0).text('O'), $('.box').eq(3).text('O'), $('.box').eq(6).text('O')],
-                [$('.box').eq(1).text('O'), $('.box').eq(4).text('O'), $('.box').eq(7).text('O')],
-                [$('.box').eq(2).text('O'), $('.box').eq(5).text('O'), $('.box').eq(8).text('O')],
-                [$('.box').eq(0).text('O'), $('.box').eq(4).text('O'), $('.box').eq(8).text('O')],
-                [$('.box').eq(2).text('O'), $('.box').eq(4).text('O'), $('.box').eq(6).text('O')]];
-var xWin = [[$('.box').eq(0).text('X'), $('.box').eq(1).text('X'), $('.box').eq(2).text('X')],
-                [$('.box').eq(3).text('X'), $('.box').eq(4).text('X'), $('.box').eq(5).text('X')],
-                [$('.box').eq(6).text('X'), $('.box').eq(7).text('X'), $('.box').eq(8).text('X')],
-                [$('.box').eq(0).text('X'), $('.box').eq(3).text('X'), $('.box').eq(6).text('X')],
-                [$('.box').eq(1).text('X'), $('.box').eq(4).text('X'), $('.box').eq(7).text('X')],
-                [$('.box').eq(2).text('X'), $('.box').eq(5).text('X'), $('.box').eq(8).text('X')],
-                [$('.box').eq(0).text('X'), $('.box').eq(4).text('X'), $('.box').eq(8).text('X')],
-                [$('.box').eq(2).text('X'), $('.box').eq(4).text('X'), $('.box').eq(6).text('X')]];
+  var winningCombos = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+              ];
+var oWin = ["O", "O", "O"];
+var xWin = ["X", "X", "X"];
 
 function checkWin() {
-  for (var i = 0; i < winArray.length; i++) {
-    //Checks possible winning combinations vs string contents
-    if (winArray[i] !== oWin && winArray !== xWin) {
-      console.log("No winner.")
+  for (var i = 0; i < winOrNot.length; i++) {
+    //This will just default to the if statement
+      if (winOrNot[i] === oWin || winOrNot[i] !== xWin) {
+        console.log("No winner!");
     }
-    else {
-      if (winArray[i] === oWin) {
-        console.log("O Wins!")
-      }
       else {
-        console.log("X wins!");
-      }
+        if (winOrNot[i] === oWin) {
+          console.log("O wins!")
+        }
+        else {
+          console.log("X wins!");
+        }
     }
   }
 }
 
-// var oWin = ["o", "o", "o"];
-// var xWin = ["x", "x", "x"];
+var board = [{}
+
+]
+
+
+// winOrNot.forEach(function(){
+//   if (winOrNot === oWin) {
+//
+//   }
+//
+// })
+
+
+// var oWin = [[$('.box').eq(0).text('O'), $('.box').eq(1).text('O'), $('.box').eq(2).text('O')],
+//                 [$('.box').eq(3).text('O'), $('.box').eq(4).text('O'), $('.box').eq(5).text('O')],
+//                 [$('.box').eq(6).text('O'), $('.box').eq(7).text('O'), $('.box').eq(8).text('O')],
+//                 [$('.box').eq(0).text('O'), $('.box').eq(3).text('O'), $('.box').eq(6).text('O')],
+//                 [$('.box').eq(1).text('O'), $('.box').eq(4).text('O'), $('.box').eq(7).text('O')],
+//                 [$('.box').eq(2).text('O'), $('.box').eq(5).text('O'), $('.box').eq(8).text('O')],
+//                 [$('.box').eq(0).text('O'), $('.box').eq(4).text('O'), $('.box').eq(8).text('O')],
+//                 [$('.box').eq(2).text('O'), $('.box').eq(4).text('O'), $('.box').eq(6).text('O')]];
+// var xWin = [[$('.box').eq(0).text('X'), $('.box').eq(1).text('X'), $('.box').eq(2).text('X')],
+//                 [$('.box').eq(3).text('X'), $('.box').eq(4).text('X'), $('.box').eq(5).text('X')],
+//                 [$('.box').eq(6).text('X'), $('.box').eq(7).text('X'), $('.box').eq(8).text('X')],
+//                 [$('.box').eq(0).text('X'), $('.box').eq(3).text('X'), $('.box').eq(6).text('X')],
+//                 [$('.box').eq(1).text('X'), $('.box').eq(4).text('X'), $('.box').eq(7).text('X')],
+//                 [$('.box').eq(2).text('X'), $('.box').eq(5).text('X'), $('.box').eq(8).text('X')],
+//                 [$('.box').eq(0).text('X'), $('.box').eq(4).text('X'), $('.box').eq(8).text('X')],
+//                 [$('.box').eq(2).text('X'), $('.box').eq(4).text('X'), $('.box').eq(6).text('X')]];
+
+// var eachBox =
+// function checkWin() {
+//   for (var i = 0; i < winArray.length; i++) {
+//     //Checks possible winning combinations vs string contents
+//     $(winOrNoto).text()
+//
+//     if (winArray[i] !== oWin && winArray !== xWin) {
+//       console.log("No winner.")
+//     }
+//     else {
+//       if (winArray[i] === oWin) {
+//         console.log("O Wins!")
+//       }
+//       else {
+//         console.log("X wins!");
+//       }
+//     }
+//   }
+// }
